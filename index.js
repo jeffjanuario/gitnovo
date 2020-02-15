ab240cba3804c42b599c6258f636973211949c1d
jeffjanuario <jefferson_januario@hotmail.com>
2020-02-15 18:47:08 -0300

    Modify api!

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res)=>{
-    res.status(200).send({message:'Status:UP'});
+app.get('/home', async (req, res, next) => {
+    res.status(200).send({ message: 'Status:HOME PAGE' });
 })
 
-app.listen(3001,()=>{
+app.get('/', async (req, res, next) => {
+    res.status(200).send({ message: 'Status:UP' });
+})
+
+app.get('*', (req, res) => {
+    if (!res.headersSent) {
+        console.log(new Error('Status:PAGE NOT FOUND'));
+        res.status(400).send({ message: 'Status:PAGE NOT FOUND' });
+    }
+})
+
+app.listen(3001, () => {
     console.log('Api initialized')
 })
